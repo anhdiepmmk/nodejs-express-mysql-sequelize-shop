6c3e28bf79243b8cbd26ecd80eb2ffd49908b972
Dieppn <dieppn@sskpi.com>
2020-11-18 08:45:12 +0700

    add connect-mongodb-session

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const path = require("path");
-
-const mongoose = require("mongoose");
-
 const express = require("express");
+const session = require("express-session");
+const MongoDBStore = require('connect-mongodb-session')(session)
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const errorController = require("./controllers/error");
 
-const app = express();
+const mongoConnectionString = "mongodb+srv://admin:admina123@cluster0.tkdbb.mongodb.net/test?retryWrites=true&w=majority";
 
+const app = express(); 
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -19,10 +22,27 @@ const User = require("./models/user");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "3MlrtFPD521GNDii",
+    cookie: {
+      maxAge: 60000
+    },
+    store: new MongoDBStore({
+      uri: mongoConnectionString,
+      collection: 'sessions'
+    })
+  })
+);
+
 app.use((req, res, next) => {
   User.findOne()
     .then((user) => {
-      req.user = user
+      req.session.user = user
+      req.user = user;
       next();
     })
     .catch((err) => {
@@ -36,12 +56,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+
+
 mongoose
-  .connect(
-    "mongodb+srv://admin:admina123@cluster0.tkdbb.mongodb.net/test?retryWrites=true&w=majority"
-  )
+  .connect(mongoConnectionString)
   .then((result) => {
-    console.log('Mongodb Connected!');
+    console.log("Mongodb Connected!");
     User.findOne()
       .then((user) => {
         if (!user) {
@@ -49,7 +69,7 @@ mongoose
             name: "Diep",
             email: "n08ni.dieppn@gmail.com",
             cart: { items: [] },
-          }).save()
+          }).save();
         }
       })
       .catch((err) => {
