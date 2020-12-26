102a032882c3fa13d84a8c716b8a2779c77c432b
Pham ngoc diep <n08ni.dieppn@gmail.com>
2020-12-26 13:22:58 +0700

    Generate file name from mimetype and uuidv4

--- src/app.js
+++ src/app.js
@@ -4,34 +4,9 @@ const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
 const path = require('path')
-const multer = require('multer')
-
-const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '..', 'public', 'images'));
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().toISOString() + '-' + file.originalname);
-    }
-});
-
-const fileFilter = (req, file, cb) => {
-    if (
-        file.mimetype === 'image/png' ||
-        file.mimetype === 'image/jpg' ||
-        file.mimetype === 'image/jpeg'
-    ) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-};
 
 // app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(
-    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
-);
 app.use(cors())
 app.use('/images', express.static(path.join(__dirname, '..', 'public', 'images')))
 
--- src/http/routes/feed.router.js
+++ src/http/routes/feed.router.js
@@ -1,6 +1,31 @@
 const express = require('express')
 const router = express.Router()
 const { body } = require('express-validator')
+const path = require('path')
+const multer = require('multer')
+const { v4: uuidv4 } = require('uuid');
+
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '..', '..', '..', 'public', 'images'));
+    },
+    filename: (req, file, cb) => {
+        cb(null, uuidv4() + '.' + file.mimetype.split('/').pop());
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
 
 const feedController = require('../controllers/feed.controller')
 
@@ -9,6 +34,7 @@ router.get('/posts', feedController.getPosts)
 
 // POST /feed/post
 router.post('/post',
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'),
     [
         body('title').trim().isLength({ min: 5, max: 255 }),
         body('content').trim().isLength({ min: 5, max: 255 })
