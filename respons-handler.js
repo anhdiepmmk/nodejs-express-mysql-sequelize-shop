e645dccd1657bd6beec57716a74152805a1daeea
Pham ngoc diep <n08ni.dieppn@gmail.com>
2021-01-01 14:03:31 +0700

    add variables __filename, __dirname to es module.

--- respons-handler.js
+++ respons-handler.js
@@ -1,9 +1,15 @@
-import fs from 'fs'
+// import fs from 'fs'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const resHandler = (req, res, next) => {
-    fs.readFile('./my-page.html', 'utf8', (err, data) => {
-        res.send(data)
-    })
+    // fs.readFile('./my-page.html', 'utf8', (err, data) => {
+    //     res.send(data)
+    // })
+    res.sendFile(path.join(__dirname, 'my-page.html'))
 }
 
 export default resHandler
