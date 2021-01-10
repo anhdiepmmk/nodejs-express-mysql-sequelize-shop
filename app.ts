8170bdf83cbe7799811421ca5e9f35cc2120adb2
Pham ngoc diep <n08ni.dieppn@gmail.com>
2021-01-11 00:09:15 +0700

    Deno crud with mongodb.

--- app.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from "https://deno.land/x/oak@v6.4.1/mod.ts";
-
-const app = new Application();
-
-app.use((ctx) => {
-  ctx.response.body = "Hello World!!!";
-});
-
-await app.listen({ port: 8000 });
